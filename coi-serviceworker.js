030f1f9aea767d5abd9facac69e486953dbdc41e
xlewis1 <lewisoverall02@gmail.com>
2026-06-09 16:06:48 +0100

    Fix service worker script download corruption with manual build

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,1 +1,25 @@
-404: Not Found
+/*! coi-serviceworker v0.1.7 - MIT License - GuildHe */
+if (typeof window === 'undefined') {
+    self.addEventListener('install', () => self.skipWaiting());
+    self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+    self.addEventListener('fetch', e => {
+        if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
+        e.respondWith(
+            fetch(e.request).then(res => {
+                if (res.status === 0) return res;
+                const headers = new Headers(res.headers);
+                headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+                headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+                return new Response(res.body, { status: res.status, statusText: res.statusText, headers });
+            }).catch(err => console.error(err))
+        );
+    });
+} else {
+    (() => {
+        if (window.crossOriginIsolated) return;
+        navigator.serviceWorker.register(window.document.currentScript.src).then(reg => {
+            reg.addEventListener('updatefound', () => location.reload());
+            if (reg.active) location.reload();
+        });
+    })();
+}
